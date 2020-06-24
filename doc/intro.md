a691bf1d1790018fce822903039f1b9daf713fa0
Lucas Pedrosa Ribeiro <lucaspedrosa11@hotmail.com>
2020-06-24 18:34:29 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datascript_estudo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/datascript_estudo/core.clj
@@ -0,0 +1,18 @@
+(ns datascript-estudo.core)
+(require '[datascript.core :as d])
+
+(let [schema {:aka {:db/cardinality :db.cardinality/many}}
+        conn   (d/create-conn schema)]
+    (d/transact! conn [ { :db/id 1
+                         :name  "Lucas"
+                         :age   23
+                         :aka   ["Pedrosa", "Lucas Pedrosa Ribeiro"]}
+                       { :db/id 2
+                        :name  "Jorge"
+                        :age   40
+                        :aka   ["Jorge", "Jorgin"]}])
+    (d/q '[ :find  ?nome ?idade
+           :where [?valor :aka "Jorge"]
+           [?valor :name ?nome]
+           [?valor :age  ?idade]]
+         @conn))
